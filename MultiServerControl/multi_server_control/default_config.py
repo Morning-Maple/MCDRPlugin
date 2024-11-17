f373af8f9184d562e927c32ed0b0e5dc60f49609
Morning-Maple <morning_maple@foxmail.com>
2024-11-17 22:20:26 +0800

    feat：modify default_config.py

--- MultiServerControl/multi_server_control/default_config.py
+++ MultiServerControl/multi_server_control/default_config.py
@@ -53,20 +53,24 @@ DEFAULT_CONFIG = \
 
 PLUGIN_METADATA = "1.0.1"
 
-START_WAIT_TIME = 10
+START_WAIT_TIME = 12
 
 # 默认帮助信息
 HELP_MSG = '''{:=^50}
-§b!!msc §f- §6显示帮助信息
-§b!!msc help §f- §6显示帮助信息
-§b!!msc list §f- §6显示在册的服务器名字
-§b!!msc reload §f- §6重新载入配置
-§b!!msc sync §e<server_name> §f- §6对名为server_name的服务器进行同步
-§b!!msc start §e<server_name> §f- §6启动名为server_name的服务器
-§b!!msc stop §e<server_name> §f- §6关闭名为server_name的服务器（需要开启Rcon）
-§b!!msc show §e<server_name> §f- §6查看名为server_name的服务器信息
-§b!!msc status §e<server_name> §f- §6查询名为server_name的服务器的状态
-{:=^50}''' \
+§b!!msc  §f-- §6显示帮助信息
+§b!!msc help  §f-- §6显示帮助信息
+§b!!msc list  §f-- §6显示在册的服务器名字
+§b!!msc reload  §f-- §6重新载入配置文件
+§b!!msc restart §e<server_name>  §f-- §6对目标服务器进行重启
+§b!!msc restart §e<server_name> §bsync  §f-- §6对目标服务器进行同步并重启
+§b!!msc sync §e<server_name>  §f-- §6对目标服务器进行同步
+§b!!msc start §e<server_name>  §f-- §6启动目标服务器
+§b!!msc stop §e<server_name>  §f-- §6关闭目标服务器（需要开启Rcon）
+§b!!msc show §e<server_name>  §f-- §6查看目标服务器信息
+§b!!msc status §e<server_name>  §f-- §6查询目标服务器的状态
+{:=^50}
+§lBy：§6§lMorning_Maple
+''' \
     .format(' §b[MultiServerControl] 帮助信息 §r',
             ' §b[MultiServerControl] Version: {} §r'
             .format(PLUGIN_METADATA))
